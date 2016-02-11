fad39b222e77ea673ebb036393559875554bc9dc
Carl Hain <crhain@gmail.com>
2016-02-11 17:22:28 -0600

    Update with character bounds checking and simple enemy movement

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
+/*CRH - TO DO 2/11/2016:
+    * add simple enemy movement & edge collision (should scroll to other side of screen)
+*/
+
 //Define some global here, because why not?
 var yOffset = 83,
     xOffset = 101;
 
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = 0;  //initial x position
-    this.y = 1 * yOffset;  //initial y position
+    if(speed === undefined) { speed = 1; }
+    this.x = x;  //initial x position
+    this.y = y;  //initial y position
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -19,42 +25,67 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //move enemy
+    this.x += this.speed * dt;
+
+    //scroll enemy to other side when it runs off screen
+    if(this.x > 5){ this.x = 0; }
+
+
+
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x * xOffset, this.y * yOffset);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
-    this.x = 2 * xOffset;  //initial x position
-    this.y = 5 * yOffset;  //initial y position
+var Player = function(startX, startY) {
+    //StartX, StartY give players starting position in "tiles"
+    this.startX = startX; //initial x position
+    this.startY = startY; //initial y position
+    this.x = startX;  //current x position
+    this.y = startY;  //current y position
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(){
-    //nothing at the moment
+    
+    //check bounds
+    if(this.x > 4) { this.x = 4; }
+    else if(this.x < 0) { this.x = 0; }
+    if(this.y > 5) { this.y = 5; }
+    else if(this.y < 0) { this.y = 0; }
+    //check for water (reset) 
+    if(this.y === 0) { this.reset(); }  
+    
 };
 
 Player.prototype.render = function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x * xOffset, this.y * yOffset);
 };
 
-Player.prototype.handleInput = function(input){
-    if(input === 'left'){this.x -= xOffset;}
-    else if (input === 'right'){this.x += xOffset;}
-    else if (input === 'up'){this.y -= yOffset;}
-    else if (input === 'down'){this.y += yOffset;}
-    else{return false;}    
+Player.prototype.handleInput = function(input){    
+    if(input === 'left'){ this.x -= 1; }
+    else if (input === 'right'){ this.x += 1; }
+    else if (input === 'up'){ this.y -= 1; }
+    else if (input === 'down'){ this.y += 1; }
+    else{ return false; }    
+};
+
+Player.prototype.reset = function(){
+    this.x = this.startX;
+    this.y = this.startY;
 };
 
 
 // Now instantiate your objects.
-var player = new Player();
-var enemy1 = new Enemy();
+var player = new Player(2, 5);
+var enemy1 = new Enemy(1, 1, 3);
 // Place all enemy objects in an array called allEnemies
 allEnemies.push(enemy1);
 // Place the player object in a variable called player
